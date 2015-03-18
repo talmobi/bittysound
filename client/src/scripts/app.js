f0087f0383b92522dac4b35912ad80404171fb00
Christian Talmo <talmo.christian@gmail.com>
2015-03-18 11:45:19 +0200

    added 'retry once' functionality (fixed some tracks stuck in infinite loading)

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -2,9 +2,15 @@ SC.initialize({
   client_id: "711c21de667ecd3ea4e91721e5a4fae1"
 });
 
+var debug = true;
 var lastSound = null;
 var $lastSpan = null;
 var trackIsPlaying = false;
+
+function log(str) {
+  if (debug)
+    console.log(str);
+}
 
 var LOAD_TIMEOUT = 2000; // ms
 var loadStartedTime = null;
@@ -26,7 +32,7 @@ function play(track, $listElement) {
 
         if (this.position < 1) // once actually playing
           return;
-        console.log("Music started to play.");
+        log("Music started to play.");
         e.removeClass("icon-spin3 animate-spin");
         e.spinning = false;
       }
@@ -49,14 +55,14 @@ function play(track, $listElement) {
       // pause/resume check
       if (trackIsPlaying) {
         var pp = lastSound.position;
-        console.log("paused at " + pp);
+        log("paused at " + pp);
         lastSound._pausePosition = pp;
         lastSound.pause();
         $s.removeClass("icon-pause");
         trackIsPlaying = false;
       } else {
         var pp = lastSound._pausePosition || 0;
-        console.log("resumed at: " + pp);
+        log("resumed at: " + pp);
         lastSound.setPosition(pp);
         lastSound.play();
         trackIsPlaying = true;
@@ -64,17 +70,50 @@ function play(track, $listElement) {
       }
 
     } else { // selected new track
+
       if ($s) {
         sound.play();
         trackIsPlaying = true;
         $s.addClass("icon-pause");
       }
 
+      // retry playing sound if first attempt fails
+      (function(){
+        var mySound = sound;
+        var $myElement = $s;
+        log("setting up retry check timeout");
+        setTimeout(function() {
+          log("retry check timeout called");
+          if ($s.is($myElement)) {
+            if (mySound.position < 1 && trackIsPlaying) {
+              // if still not playing - try again
+              mySound.stop();
+              mySound.play();
+              log(" -- retrying to play track");
+
+              // set another timeout that tells the user
+              // the music is broken if it still doesn't work
+              setTimeout(function(){
+                if (mySound.position < 0 && trackIsPlaying
+                    && $s.is($myElement)) {
+                  $myElement.addClass("icon-stop");
+                  log(" !! track seems to be broken, tell user and stop trying");
+                }
+              }, LOAD_TIMEOUT + 500);
+            } else {
+              log("element OK but check failed");
+              log("  position is: " + mySound.position);
+              log("  trackIsPlaying is: " + trackIsPlaying);
+            }
+          } else {
+            log(" << NOT SAME ELEMENT >> ");
+          }
+        }, LOAD_TIMEOUT);
+      })();
+
       lastSound = sound;
+      $lastSpan = $s;
     }
-
-    $lastSpan = $s;
-
   });
 }
 
@@ -89,8 +128,8 @@ function addMoreTracks(amount) {
 
   var tracks = lastTracks;
 
-  console.log("current: " + currentTrackIndex);
-  console.log("tracks.length: " + tracks.length);
+  log("current: " + currentTrackIndex);
+  log("tracks.length: " + tracks.length);
 
   if (currentTrackIndex >= tracks.length) {
     showMessage("<b>Didn't find any more songs!</b> Try a new search?", "error");
@@ -117,7 +156,7 @@ function addMoreTracks(amount) {
     (function(){
       var e = el;
       ii.on('click', function () {
-        console.log("click: " + e.trackUri);
+        log("click: " + e.trackUri);
         play(e.track, e);
         return false;
       })
@@ -130,7 +169,7 @@ function addMoreTracks(amount) {
   currentTrackIndex = limit;
 }
 
-var defaultLimit = 12;
+var defaultLimit = 16;
 var $text = $('#message-text');
 
 function search(str) {
@@ -147,37 +186,39 @@ function search(str) {
     lastTracks = tracks;
     $('#more-button').html("More results").removeClass().addClass('icon-plus');
     var track = tracks[0];
+
     if (tracks.length < 1) {
-      console.log("No tracks found!");
+      log("No tracks found!");
       $list.empty();
-      $('.results-footer').css("display", "none");
+      $('.results-footer').css("display", "none"); // hide more button
       showMessage("<b>Didn't find any songs!</b> Try a new search?", "error");
     } else {
       showMessage("<b>Here are some results.</b>", "ok");
 
-      // clear list
+      // clear list for new search
       $list.empty();
 
+      // Initial amount of tracks to show after search
       addMoreTracks(4);
-      $('.results-footer').css("display", "block");
+      $('.results-footer').css("display", "block"); // show more button
     }
-  }); // SC.get
+  }); // eof SC.get
 }
 
 // default debug search result
 search("melody circus");
 
-var defaultText = "Search for any song";
+var inputWatermarkText = "Search for any song";
 var input = $('.searchbar input');
 
 // setup input watermark
-input.val(defaultText);
+input.val(inputWatermarkText);
 input.on('focus', function () {
   input.val("");
 });
 input.on('blur', function () {
   if (input.val().length < 1)
-    input.val(defaultText);
+    input.val(inputWatermarkText);
 });
 
 var timeout = null;
@@ -209,7 +250,7 @@ $('#more-button').on('click', function() {
 var $text = $('#message-text');
 var $message = $('.message');
 function showMessage(message, type) {
-  console.log("showmessage called");
+  log("showmessage called");
   $text.removeClass();
   $text.html(message);
 
@@ -217,4 +258,4 @@ function showMessage(message, type) {
   $message.addClass("message info-ok info-" + type);
 }
 
-console.log("app loaded");
+log("app loaded");
